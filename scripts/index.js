0ec21733cf1722d6797e076cfea53e59559267a2
Kival Evan <mnabawiazizul@gmail.com>
2021-03-16 16:24:08 +0800

    plenty o stuff
    commission map showcase
    some js stuff to deal with nav
    mobile css stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,32 @@
 // if user has JS disabled, then section should always show
 // otherwise, hide and only show specific
-$('section').hide();
+// replace section id with something else to prevent href redirect
+// this is because if user has JS enabled, then they shouldnt move anywhere and only replace content
+
+$('section')
+    .hide()
+    .each(function () {
+        $(this).prop('id', `section-${$(this).attr('id')}`);
+    });
+
 if (location.hash !== null && location.hash !== '') {
     const section = ['home', 'project', 'commission', 'contact'];
     const index = section.indexOf(location.hash.substring(1));
     if (index !== -1) {
         $('nav li').find(`selector-${section[index]}`).addClass('active');
-        $('section#' + section[index]).show();
+        $(`section#section-${section[index]}`).show();
+        document.title = `Kival Evan | ${
+            section[index].charAt(0).toUpperCase() + section[index].slice(1)
+        }`;
     } else {
         $('nav li a').first().addClass('active');
         $('section').first().show();
+        document.title = 'Kival Evan | Home';
     }
 } else {
     $('nav li a').first().addClass('active');
     $('section').first().show();
+    document.title = 'Kival Evan | Home';
 }
 
 $('nav li a').click(function () {
@@ -23,8 +36,9 @@ $('nav li a').click(function () {
         .siblings('li')
         .children()
         .removeClass('active');
-    $('section#' + $(this).attr('id').substring(9))
+    $(`section#section-${$(this).attr('id').substring(9)}`)
         .show()
         .siblings('section')
         .hide();
+    document.title = `Kival Evan | ${$(this).html()}`;
 });
