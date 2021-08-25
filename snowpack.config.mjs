36cd112048076e838cb2fa539de3620b6432d9e5
Kival Evan <mnabawiazizul@gmail.com>
2021-08-25 15:08:05 +0800

    a

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,3 +1,9 @@
 export default {
+    alias: {
+        $: './src',
+        $data: './src/data',
+        $components: './src/components',
+        $layouts: './src/layouts',
+    },
     plugins: ['@snowpack/plugin-typescript', '@snowpack/plugin-sass'],
 };
