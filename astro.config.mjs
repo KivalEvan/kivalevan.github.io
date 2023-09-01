7d8fb7d694557fefea88256622b1ebbe85f04bdd
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2023-09-01 15:52:45 +0800

    hyrewhreehr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import svelte from '@astrojs/svelte';
-import image from '@astrojs/image';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://kivalevan.me',
-    base: '/',
-    integrations: [svelte(), image()],
+  site: 'https://kivalevan.me',
+  base: '/',
+  integrations: [svelte()]
 });
