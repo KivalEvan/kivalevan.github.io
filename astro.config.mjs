f6f02d94d04460124bee2b5c15197f21380885e2
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-05-15 19:36:05 +0800

    comm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://kivalevan.me',
   base: '/',
   integrations: [svelte()],
   image: {
-    service: passthroughImageService(),
-  },
+    service: passthroughImageService()
+  }
 });
